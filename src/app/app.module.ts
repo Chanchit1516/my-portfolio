a4f46e21641601eeeafa406aea15b5860cf82cdb
chanchit <chanchit.ler01@gmail.com>
2022-09-10 02:25:13 +0700

    Create chatbot component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ChatComponent } from './shared/chat/chat.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     MainLayoutComponent,
-    NavbarComponent
+    NavbarComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
