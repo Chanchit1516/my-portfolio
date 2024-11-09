71e29ea476916efa9f39645c6afd49d2aa9778c2
chanchit <chanchit.ler01@gmail.com>
2024-11-09 18:41:59 +0700

    Create a new function to get age by day, month, year

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -138,6 +138,23 @@ export class HomePageComponent implements OnInit {
     link.remove();
   }
 
+  getAge() {
+    const day = 17;
+    const month = 10;
+    const year = 1995;
+    const today = new Date();
+    const birthDate = new Date(year, month - 1, day);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    const dayDiff = today.getDate() - birthDate.getDate();
+  
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+      age--;
+    }
+  
+    return age;
+  }
+
 
 
 
